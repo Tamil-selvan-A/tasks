940db012fd0edfed91c38df96cfd7ed0dfc9d8d8
Tamil selvan <73411756+Tamil-selvan-A@users.noreply.github.com>
2021-09-19 19:32:14 +0530

    Add files via upload

---
+++ Phase 2/Assignment-4__08082020_08102020/assets/javascript/script.js
@@ -0,0 +1,11 @@
+/* RESPONSIVE MENU */ 
+document.querySelector('.fa-bars').addEventListener('click', nav);
+function nav() {
+    document.getElementById('nav').classList.toggle('responsive-nav');
+}
+
+
+
+
+
+
